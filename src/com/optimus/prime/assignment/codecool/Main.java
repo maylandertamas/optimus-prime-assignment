82219ec460b5a283f8b270ed6c562c65a3be921e
maylandertamas <maylandertamas@gmail.com>
2017-08-15 18:08:49 +0200

    Done solution

--- src/com/optimus/prime/assignment/codecool/Main.java
+++ src/com/optimus/prime/assignment/codecool/Main.java
@@ -12,16 +12,15 @@ import java.util.stream.IntStream;
 public class Main {
 
     static void findPrimes(int maxNum) {
-        List<Integer> numberList = IntStream.range(2, maxNum+1)
-                .filter(l -> (l % 2 != 0 && l % 3 != 0 && l % 5 != 0 && l % 7 != 0))
-                .boxed().collect(Collectors.toList());
-        System.out.println(numberList);
+        System.out.println(IntStream.range(2, maxNum+1)
+                .filter(l -> l != 2 && l != 3 && l != 5 && l != 7?(l % 2 != 0 && l % 3 != 0 && l % 5 != 0 && l % 7 != 0): l % 1 == 0)
+                .boxed().collect(Collectors.toList()));
      }
 
 
 
     public static void main(String[] args) {
-        findPrimes(30);
+        findPrimes(120);
 
     }
 }
