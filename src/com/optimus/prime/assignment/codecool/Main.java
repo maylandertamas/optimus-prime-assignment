2bb87952c3e7cc55e4db99a3b9c32eff6c265583
maylandertamas <maylandertamas@gmail.com>
2017-08-15 18:53:24 +0200

    Fixed outcome: count primes

--- src/com/optimus/prime/assignment/codecool/Main.java
+++ src/com/optimus/prime/assignment/codecool/Main.java
@@ -7,18 +7,14 @@ public class Main {
 
     static void findPrimes(int maxNum) {
         System.out.println(IntStream.range(2, maxNum+1)
-                .filter(l -> l != 2 && l != 3 && l != 5 && l != 7?(l % 2 != 0 && l % 3 != 0 && l % 5 != 0 && l % 7 != 0): l % 1 == 0)
-                .boxed().collect(Collectors.toList()));
-        long tStart = System.nanoTime();
-        long tEnd = System.nanoTime();
-        long tRes = tEnd - tStart; // time in nanoseconds
-        System.out.println(tRes);
+                .filter(l -> l != 2 && l != 3 && l != 5 && l != 7?(l % 2 != 0 && l % 3 != 0 && l % 5 != 0 && l % 7 != 0): l % 1 == 0).count());
+                //.boxed().collect(Collectors.toList()));
      }
 
 
 
     public static void main(String[] args) {
-        findPrimes(120);
+        findPrimes(30);
 
     }
 }
