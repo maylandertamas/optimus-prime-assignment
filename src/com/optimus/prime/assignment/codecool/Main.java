811e59454fc70adf12ddd1470d2fec300436ebb7
maylandertamas <maylandertamas@gmail.com>
2017-08-15 18:41:19 +0200

    Testing

--- src/com/optimus/prime/assignment/codecool/Main.java
+++ src/com/optimus/prime/assignment/codecool/Main.java
@@ -1,11 +1,5 @@
 package com.optimus.prime.assignment.codecool;
 
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -15,6 +9,10 @@ public class Main {
         System.out.println(IntStream.range(2, maxNum+1)
                 .filter(l -> l != 2 && l != 3 && l != 5 && l != 7?(l % 2 != 0 && l % 3 != 0 && l % 5 != 0 && l % 7 != 0): l % 1 == 0)
                 .boxed().collect(Collectors.toList()));
+        long tStart = System.nanoTime();
+        long tEnd = System.nanoTime();
+        long tRes = tEnd - tStart; // time in nanoseconds
+        System.out.println(tRes);
      }
 
 
