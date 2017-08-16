d0d24e54d07c4d06d09e910248af8aef139dec78
maylandertamas <maylandertamas@gmail.com>
2017-08-16 15:58:08 +0200

    Changed everything

--- src/com/optimus/prime/assignment/codecool/Main.java
+++ src/com/optimus/prime/assignment/codecool/Main.java
@@ -1,18 +1,32 @@
 package com.optimus.prime.assignment.codecool;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.*;
 
 public class Main {
 
-    static long findPrimes(int maxNum) {
-        return (int)IntStream.range(2, (maxNum+1))
+    static void findPrimes(int maxNum) {
+        int[] primes = new int[maxNum];
+        Arrays.fill(primes, 1);
+        int k = 2;
+        for (int i = 2; i < primes.length; i++) {
+            Arrays.stream(primes).map(z -> ){
+
+            }):z*1);
+        }
+
+
+
+        /*IntStream.range(2, (maxNum+1))
                 .filter(l -> l != 2 && l != 3 && l != 5 && l != 7?(l % 2 != 0 && l % 3 != 0 && l % 5 != 0 && l % 7 != 0): l % 1 == 0).count();
-                //.boxed().collect(Collectors.toList()));
+                //.boxed().collect(Collectors.toList()));*/
      }
 
     public static void main(String[] args) {
-        System.out.println(findPrimes(120));
+        findPrimes(120);
     }
 
 }
