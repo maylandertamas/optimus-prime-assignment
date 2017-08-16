6293bd5c4f0a3197c34bbcf24bbe69a7c98b9a23
maylandertamas <maylandertamas@gmail.com>
2017-08-16 12:22:42 +0200

    Added cases to test

--- src/com/optimus/prime/assignment/codecool/Main.java
+++ src/com/optimus/prime/assignment/codecool/Main.java
@@ -5,16 +5,16 @@ import java.util.stream.IntStream;
 
 public class Main {
 
-    static void findPrimes(int maxNum) {
-        System.out.println(IntStream.range(2, maxNum+1)
-                .filter(l -> l != 2 && l != 3 && l != 5 && l != 7?(l % 2 != 0 && l % 3 != 0 && l % 5 != 0 && l % 7 != 0): l % 1 == 0).count());
+    static long findPrimes(int maxNum) {
+        return IntStream.range(2, maxNum+1)
+                .filter(l -> l != 2 && l != 3 && l != 5 && l != 7?(l % 2 != 0 && l % 3 != 0 && l % 5 != 0 && l % 7 != 0): l % 1 == 0).count();
                 //.boxed().collect(Collectors.toList()));
      }
 
 
 
     public static void main(String[] args) {
-        findPrimes(30);
+        System.out.println(findPrimes(124124124));
 
     }
 }
--- src/com/optimus/prime/assignment/codecool/MainTest.java
+++ src/com/optimus/prime/assignment/codecool/MainTest.java
@@ -1,7 +1,26 @@
 package com.optimus.prime.assignment.codecool;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MainTest {
+    @Test
+    public void testPrimeFinder1()
+    {
+        long expected = 10;
+        assertEquals("with the range of 30 the expected result should be 10", expected, Main.findPrimes(30));
+    }
 
+    @Test
+    public void testPrimeFinder2() {
+        long expected = 30;
+        assertEquals("with the range of 120 the expected result should be 30", expected, Main.findPrimes(120));
+    }
+
+    @Test
+    public void testPrimeFinder3() {
+        long expected = new Integer(29);
+        assertEquals("with the range of 110 the expected result should be 29", expected, Main.findPrimes(110));
+    }
 }
