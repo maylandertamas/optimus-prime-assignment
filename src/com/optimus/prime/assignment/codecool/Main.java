aa17dbfb67fe99a3f1f546e8bda2f1b7b40100ba
maylandertamas <maylandertamas@gmail.com>
2017-08-17 11:17:54 +0200

    Added new test cases and tested new solution

--- src/com/optimus/prime/assignment/codecool/Main.java
+++ src/com/optimus/prime/assignment/codecool/Main.java
@@ -1,12 +1,9 @@
 package com.optimus.prime.assignment.codecool;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.*;
 
 public class Main {
 
@@ -14,20 +11,18 @@ public class Main {
 
         List<Integer> startList = IntStream.range(2, (maxNum+1))
                 .filter(l -> l != 2 && l != 3 && l != 5 && l != 7?(l % 2 != 0 && l % 3 != 0 && l % 5 != 0 && l % 7 != 0): l % 1 == 0).boxed().collect(Collectors.toList());
-        System.out.println(startList);
 
         for (int i = 4; i < startList.size(); i++) {
             List<Integer> temp = new ArrayList<>(startList);
             int finalI = i;
             startList = temp.stream().filter(l -> l != temp.get(finalI) ?(l % temp.get(finalI) != 0): l % 1 == 0).collect(Collectors.toList());
         }
-        System.out.println(startList);
-        //startList = temp.stream().forEach(l -> l != temp.get(finalI) && l % finalI == 0? (hehe.add()));
+        System.gc();
         return startList.size();
      }
 
     public static void main(String[] args) {
-        findPrimes(130);
+        System.out.println(findPrimes(1151));
     }
 
 }
--- src/com/optimus/prime/assignment/codecool/MainTest.java
+++ src/com/optimus/prime/assignment/codecool/MainTest.java
@@ -24,4 +24,22 @@ public class MainTest {
         int expected = 29;
         assertEquals("with the range of 110 the expected result should be 29", expected, Main.findPrimes(110));
     }
+
+    @Test
+    public void testPrimeFinder5() {
+        int expected = 31;
+        assertEquals("with the range of 130 the expected result should be 31", expected, Main.findPrimes(130));
+    }
+
+    @Test
+    public void testPrimeFinder6() {
+        int expected = 62;
+        assertEquals("with the range of 300 the expected result should be 62", expected, Main.findPrimes(300));
+    }
+
+    @Test
+    public void testPrimeFinder7() {
+        int expected = 190;
+        assertEquals("with the range of 1151 the expected result should be 190", expected, Main.findPrimes(1151));
+    }
 }
